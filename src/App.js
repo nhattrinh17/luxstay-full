293ef50cbbb787073af3db419d4da7e8afafffda
{TrinhNhat} <{trinhminhnhatxt123@gmail.com}>
2022-05-12 00:38:01 +0700

    Finish Slider and Welcome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+
 import './App.css';
 import Header from './Components/Header/Header';
+import Slider from './Components/Slider/Slider';
+import Welcome from './Components/Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Routes>
+        <Route path='/' 
+              element={<React.Fragment>
+                        <Header/>
+                        <Slider/> 
+                        <Welcome/>
+                      </React.Fragment>}/>
+
+      </Routes>
+      
     </div>
   );
 }
